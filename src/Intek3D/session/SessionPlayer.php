ce0364eec03dce67c524d591c50da35a0e4b20c0
jonattanzapata144@gmail.com <jonattanzapata144@gmail.com>
2023-06-12 19:46:55 -0500

    adds

--- src/Intek3D/session/SessionPlayer.php
+++ src/Intek3D/session/SessionPlayer.php
@@ -7,8 +7,10 @@ use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
 use pocketmine\Server;
+use pocketmine\level\sound\GhastShootSound;
 
 use Intek3D\session\SessionLoader;
+use Inten3D\BOB;
 
 class SessionPlayer implements SessionLoader {
   
@@ -16,7 +18,22 @@ class SessionPlayer implements SessionLoader {
   public function onPlayerJoinEvent(PlayerJoinEvent $event){
     $player = $event->getPlayer();
     $nick = $player->getNameTag();
-    
+    $config = BOB::getInstance()->getConfig();
+    $mensaje = str_replace("{nick}", $nick, $config->get("player-join"));
+    $event->setJoinMessage($mensaje);
+    $player->sendTitle($config->get("join-title"));
+    $player->sendMessage($config->get("welcome-message"));
+    $player->setFood(20);
+    $player->setHealth(20);
+    $player->getLevel()->addSound(new GhastShootSound($player));
+    $player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+  }
+  public function onPlayerQuitEvent(PlayerQuitEvent $event){
+    $player = $event->getPlayer();
+    $nick = $player->getNameTag();
+    $config = BOB::getInstance()->getConfig();
+    $mensaje = str_replace("{nick}", $nick, $config->get("player-leave"));
+    $event->setQuitMessage($mensaje);
     
   }
 }
