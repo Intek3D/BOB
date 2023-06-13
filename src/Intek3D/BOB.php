32b9031dfb5d073f902385614aff69d05898d81e
jonattanzapata144@gmail.com <jonattanzapata144@gmail.com>
2023-06-12 19:29:16 -0500

    Adds

---
+++ src/Intek3D/BOB.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Intek3D;
+
+use Intek3D\session\SessionLoader;
+
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase; 
+use pocketmine\utils\SingletonTrait;
+
+class BOB extends PluginBase {
+  
+ use SingletonTrait;
+ 
+ protected function onLoad(): void {
+   self::setInstance($this);
+    }
+ public function onEnable(): void {
+   $config = BOB::getInstance()->getConfig();
+   $this->getLogger()->info("§aTodo Funcionando Correctamente!");
+   $this->saveResource("config.yml");
+   $this->getServer()->getNetwork()->setName($config->get("motd-server"));
+  }
+  public function onDisable(): void {
+    $this->getLogger()->info("§cTodo fue guardado correctamente!");
+  }
+ }
---
+++ src/Intek3D/session/SessionPlayer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Intek3D\session;
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+use pocketmine\Server;
+
+use Intek3D\session\SessionLoader;
+
+class SessionPlayer implements SessionLoader {
+  
+}
